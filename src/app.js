28840341636331e71ab6af9e2c864b3c8e18e6e3
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-01 15:34:26 -0300

    criando rota padrao

--- src/app.js
+++ src/app.js
@@ -1,3 +1,8 @@
 const express = require('express')// importar o express
 const app = express()// criar uma instancia
 const port = 3000// define a porta
+
+// criar rota padrao ou raiz
+app.get('/', (req, res) => {
+    res.send('Olá Mundo!')
+})
