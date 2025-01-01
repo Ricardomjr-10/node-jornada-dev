b0117c01d1a0a6f05fff4aa10dba15b7a0282542
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-01 16:07:58 -0300

    add rota selecoes e mock de dados array de selecoes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import app from '.src/app.js'//importar o app
-const port = 3000// define a porta
+import app from './src/app.js'//importar o app
+const PORT = 3000// define a porta
 
 // escutar porta
-app.listen(port, () => {
-    console.log(`Servvidor rodando no endereco http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Servvidor rodando no endereco http://localhost:${PORT}`)
 })
--- src/app.js
+++ src/app.js
@@ -3,9 +3,22 @@ import express from 'express'// importar o express
 const app = express()// criar uma instancia
 
 
+// mock de dados
+const selecoes = [
+    { id: 1, selecao: 'Brasil', grupo: 'G' },
+    { id: 2, selecao: 'Suiça', grupo: 'G' },
+    { id: 3, selecao: 'Sérvia', grupo: 'G' },
+    { id: 4, selecao: 'Camarões', grupo: 'G' }
+]
 // criar rota padrao ou raiz
 app.get('/', (req, res) => {
     res.send('Curso de NodeJS')
 })
+
+//nova rota, endpoint
+app.get('/selecoes', (req, res) => {
+    res.send('Lista de seleções')//enviar uma resposta
+})
+
 //exportar o app
 export default app
