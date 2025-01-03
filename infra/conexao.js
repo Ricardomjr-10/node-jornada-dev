e4ede89f62bb7f487c36f62e8dea446e81d9e32d
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-02 22:53:11 -0300

    importando conexao no arquivo server

--- infra/conexao.js
+++ infra/conexao.js
@@ -3,8 +3,8 @@ import mysql from 'mysql'
 const conexao = mysql.createConnection({
     host: 'localhost',
     port: '3306',
-    user: 'root',
-    password: 'root',
+    user: 'dba',
+    password: '1234',
     database: 'bdcopa'
 })
 
