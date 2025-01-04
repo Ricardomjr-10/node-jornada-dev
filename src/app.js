6944210a39a1ac300939a38f0678d3799031e74a
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-03 23:39:34 -0300

    alterando array de selecoes

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const selecoes = [
     { id: 6, selecao: 'Uruguai', grupo: 'F' },
     { id: 7, selecao: 'Alemanha', grupo: 'F' },
     { id: 8, selecao: 'Argentina', grupo: 'F' },
+    { id: 9, selecao: 'Espanha', grupo: 'F' },
 ]
 
 //funcao para localizar selecoes por id
