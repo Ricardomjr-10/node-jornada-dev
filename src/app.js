67fc2c1c0066ac7c194541f3b34ee9530c489917
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-04 21:32:12 -0300

    limpando o codigo

--- src/app.js
+++ src/app.js
@@ -4,24 +4,12 @@ const app = express()// criar uma instancia
 
 app.use(express.json())// configurar o app para receber json-ler body com json
 
-
-//funcao para localizar selecoes por id
-function buscarSelecaoPorId(id) {
-    return selecoes.filter(selecao => selecao.id == id)
-}
-
-//funcao para buscar selecao pelo indice
-function buscarIndexSelecao(id) {
-    return selecoes.findIndex(selecao => selecao.id == id)
-}
-
-
 //nova rota, endpoint
 app.get('/selecoes', (req, res) => {
     const sql = 'SELECT * FROM selecoes'
     conexao.query(sql, (erro, resultado) => {
         if (erro) {
-            res.status(404).json({'erro': erro})
+            res.status(404).json({ 'erro': erro })
         } else {
             res.status(200).json(resultado)
         }
@@ -35,11 +23,11 @@ app.get('/selecoes/:id', (req, res) => {
     conexao.query(sql, id, (erro, resultado) => {
         const linha = resultado[0]
         if (erro) {
-            res.status(404).json({'erro': erro})
+            res.status(404).json({ 'erro': erro })
         } else {
             res.status(200).json(linha)
         }
-        
+
     })
 })
 
@@ -47,9 +35,9 @@ app.get('/selecoes/:id', (req, res) => {
 app.post('/selecoes', (req, res) => {
     const selecao = req.body
     const sql = 'INSERT INTO selecoes SET ?' // ? CONJUNTO DE DADOS QUE VAI SER INSERIDO
-    conexao.query(sql,selecao, (erro, resultado) => {
+    conexao.query(sql, selecao, (erro, resultado) => {
         if (erro) {
-            res.status(400).json({'erro': erro})
+            res.status(400).json({ 'erro': erro })
         } else {
             res.status(201).json(resultado)
         }
@@ -62,7 +50,7 @@ app.delete('/selecoes/:id', (req, res) => {
     const sql = 'DELETE FROM selecoes WHERE id = ?'
     conexao.query(sql, id, (erro, resultado) => {
         if (erro) {
-            res.status(404).json({'erro': erro})
+            res.status(404).json({ 'erro': erro })
         } else {
             res.status(200).json(resultado)
         }
@@ -74,9 +62,9 @@ app.put('/selecoes/:id', (req, res) => {
     const id = req.params.id
     const selecao = req.body
     const sql = 'UPDATE selecoes SET ? WHERE id = ?'
-    conexao.query(sql,[selecao, id], (erro, resultado) => {
+    conexao.query(sql, [selecao, id], (erro, resultado) => {
         if (erro) {
-            res.status(400).json({'erro': erro})
+            res.status(400).json({ 'erro': erro })
         } else {
             res.status(200).json(resultado)
         }
