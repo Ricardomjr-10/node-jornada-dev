14cbee8ddbc33ea1585f44e6e03f52b1d87e2d5a
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-06 20:50:55 -0300

    refatorando metodos no repository

--- src/app/database/conexao.js
+++ src/app/database/conexao.js
@@ -10,4 +10,23 @@ const conexao = mysql.createConnection({
 // conectar ao banco
 conexao.connect()
 
+//jsdoc
+/**
+ * Exexuta um codigo sql com ou sem valores
+ * @param {string} sql instrucao sql a ser executada
+ * @param {string=id | [selecao, id]} valores // valores a serem passados para o sql
+ * @param {*} mensagemReject 
+ * @returns 
+ */
+const consulta = (sql, valores='', mensagemReject) => {
+    return new Promise((resolve, reject) => {
+        conexao.query(sql, selecao, (erro, resultado) => {
+            if (erro) return reject('Não foi possível cadastrar')
+            //fazer o parse dos resultados
+            const row = JSON.parse(JSON.stringify(resultado))
+            return resolve(row)
+        })
+    })
+}
+
 export default conexao
