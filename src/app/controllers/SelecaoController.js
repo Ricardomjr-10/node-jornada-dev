6575346cf6a1e390117bf541a90eb39d722feee8
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-05 21:54:08 -0300

    build func async await metodos show and store

--- src/app/controllers/SelecaoController.js
+++ src/app/controllers/SelecaoController.js
@@ -6,29 +6,16 @@ class SelecaoController {
         const row = await SelecaoRepository.findAll()//await - aguardar a reposta
         res.json(row)
     } // listar tudo
-    show(req, res) {
+    async show(req, res) {
         const id = req.params.id
-        const sql = 'SELECT * FROM selecoes WHERE id = ?'
-        conexao.query(sql, id, (erro, resultado) => {
-            const linha = resultado[0]
-            if (erro) {
-                res.status(404).json({ 'erro': erro })
-            } else {
-                res.status(200).json(linha)
-            }
-    
-        })
+       const row = await SelecaoRepository.findById(id)
+       res.json(row)
     } // listar por id
-    store(req, res) {
+    async store(req, res) {
         const selecao = req.body
-        const sql = 'INSERT INTO selecoes SET ?' // ? CONJUNTO DE DADOS QUE VAI SER INSERIDO
-        conexao.query(sql, selecao, (erro, resultado) => {
-            if (erro) {
-                res.status(404).json({ 'erro': erro })
-            } else {
-                res.status(201).json(resultado)
-            }
-        })
+        const row = await SelecaoRepository.create(selecao)
+        res.json(row)
+        
     } // criar dados
     update(req, res) {
         const id = req.params.id
--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -2,7 +2,17 @@ import conexao from '../database/conexao.js'
 
 class SelecaoRepository {
     //CRUD
-    create() {}
+    create() {
+        const sql = 'INSERT INTO selecoes SET ?' // ? CONJUNTO DE DADOS QUE VAI SER INSERIDO
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, (erro, resultado) => {
+                if (erro) return reject('Não foi possível criar a seleção')
+                    //fazer o parse dos resultados
+                    const row = JSON.parse(JSON.stringify(resultado))
+                    return resolve(row)
+            })
+        })
+    }
     findAll() {
         const sql = 'SELECT * FROM selecoes'
         return new Promise((resolve, reject) => {
@@ -14,7 +24,17 @@ class SelecaoRepository {
             })
         })
     }
-    findById() {}
+    findById(id) {
+        const sql = 'SELECT * FROM selecoes WHERE id = ?'
+        return new Promise((resolve, reject) => {
+            conexao.query(sql, id, (erro, resultado) => {
+                if (erro) return reject('Não foi possível listar as seleções')
+                    //fazer o parse dos resultados
+                    const row = JSON.parse(JSON.stringify(resultado))
+                    return resolve(row)
+            })
+        })
+    }
     update() {}
     delete() {}
 }
