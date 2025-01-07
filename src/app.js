c18f5da99451634c08ee539d7972c436a052aa31
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-06 21:37:56 -0300

    atualizando ordem dos scripts do arquivo app.js

--- src/app.js
+++ src/app.js
@@ -3,10 +3,10 @@ import routes from './routes.js'
 
 const app = express()// criar uma instancia
 
+app.use(express.json())// configurar o app para receber json-ler body com json
+
 //usar o routers
 app.use(routes)
-
-app.use(express.json())// configurar o app para receber json-ler body com json
 
 //exportar o app
 export default app
