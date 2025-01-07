fbc02b69c243b3ff419a0bdf8d80d21f88ce1b33
Ricardo-10 <ricardomjr2@gmail.com>
2025-01-06 21:01:01 -0300

    refatorado o codigo do repository

--- src/app/repositories/SelecaoRepository.js
+++ src/app/repositories/SelecaoRepository.js
@@ -1,43 +1,22 @@
-import conexao from '../database/conexao.js'
+import conexao, {consulta} from '../database/conexao.js'
 
 class SelecaoRepository {
     //CRUD
     create(selecao) {
         const sql = 'INSERT INTO selecoes SET ?' // ? CONJUNTO DE DADOS QUE VAI SER INSERIDO
-        
+        return consulta(sql, selecao, 'Não foi possivel cadastrar')
     }
     findAll() {
         const sql = 'SELECT * FROM selecoes'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, (erro, resultado) => {
-                if (erro) return reject('Não foi possível listar as seleções')
-                //fazer o parse dos resultados
-                const row = JSON.parse(JSON.stringify(resultado))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, 'Não foi possivel localizar')
     }
     findById(id) {
         const sql = 'SELECT * FROM selecoes WHERE id = ?'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, id, (erro, resultado) => {
-                if (erro) return reject('Não foi possível listar as seleções')
-                //fazer o parse dos resultados
-                const row = JSON.parse(JSON.stringify(resultado))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, id, 'Não foi possivel localizar')
     }
     update(selecao, id) {
         const sql = 'UPDATE selecoes SET ? WHERE id = ?'
-        return new Promise((resolve, reject) => {
-            conexao.query(sql, [selecao, id], (erro, resultado) => {
-                if (erro) return reject('Não foi possível atualizar a seleção')
-                //fazer o parse dos resultados
-                const row = JSON.parse(JSON.stringify(resultado))
-                return resolve(row)
-            })
-        })
+        return consulta(sql, )
     }
     delete(id) {
         const sql = 'DELETE FROM selecoes WHERE id = ?'
